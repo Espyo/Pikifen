89855d8728eccabe8648efb6729da5eb81f71431
Andre 'Espyo' Silva <andreluis.g.silva@gmail.com>
2013-09-07 20:40:17 +0100

    Simple test commit 2.

--- Source/const.h
+++ Source/const.h
@@ -3,8 +3,8 @@
 
 #define CURSOR_MAX_DIST           300    //The cursor can only be these many pixels away from the captain.
 #define GRAVITY_ADDER             -0.1f  //Accelerate the Z speed of mobs affected by gravity by this amount per second.
-#define HEALTH_CIRCLE_RADIUS      20
-#define LEADER_MOVE_SPEED         100
+#define HEALTH_CIRCLE_RADIUS      20     //Radius of the health meter's circle.
+#define LEADER_MOVE_SPEED         100    //Max speed at which a leader can move.
 #define MAX_WHISTLE_RADIUS        80     //The whistle can't go past this radius.
 #define MIN_PIKMIN_GRABBING_RANGE 60     //The leader needs to be at least this close to a Pikmin to grab it.
 #define MIN_PLUCK_RANGE           30     //The leader needs to be at least this close to a burrowed Pikmin to pluck it.
