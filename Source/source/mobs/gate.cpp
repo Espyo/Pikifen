e426df484a83c9510e93161ace55d5ddc897df06
Espyo <andreluis.g.silva@gmail.com>
2019-01-10 23:09:44 +0000

    Updated code file copyright information.
    - Removed the second part of the date, so I don't have to update it every year.
    - Condensed the existing text into less lines.

--- Source/source/mobs/gate.cpp
+++ Source/source/mobs/gate.cpp
@@ -1,8 +1,7 @@
 /*
- * Copyright (c) Andre 'Espyo' Silva 2013-2018.
- * The following source file belongs to the open-source project
- * Pikifen. Please read the included
- * README and LICENSE files for more information.
+ * Copyright (c) Andre 'Espyo' Silva 2013.
+ * The following source file belongs to the open-source project Pikifen.
+ * Please read the included README and LICENSE files for more information.
  * Pikmin is copyright (c) Nintendo.
  *
  * === FILE DESCRIPTION ===
