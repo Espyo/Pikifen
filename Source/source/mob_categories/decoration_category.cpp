91b2025a60e897e84f2894553a9269112defad97
Espyo <andreluis.g.silva@gmail.com>
2019-04-25 23:00:45 +0100

    Added a bunch of the new objects to Play.
    - Increased the radii of the Spiderworts so they're not so tiny.
    - Changed the color of decorative objects in the area editor so they don't pop out as much.

--- Source/source/mob_categories/decoration_category.cpp
+++ Source/source/mob_categories/decoration_category.cpp
@@ -22,7 +22,7 @@
 decoration_category::decoration_category() :
     mob_category(
         MOB_CATEGORY_DECORATIONS, "Decoration", "Decorations",
-        DECORATIONS_FOLDER_PATH, al_map_rgb(137, 215, 134)
+        DECORATIONS_FOLDER_PATH, al_map_rgb(160, 180, 160)
     ) {
     
 }
