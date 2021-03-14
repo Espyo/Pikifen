22f55d862b3324be00a34b1b2f83eba896b4176b
Superkman23 <43509068+Superkman23@users.noreply.github.com>
2021-03-14 18:45:46 -0400

    Changes to allow compiling though Visual Studio

--- Source/source/utils/math_utils.cpp
+++ Source/source/utils/math_utils.cpp
@@ -150,5 +150,5 @@ int sum_and_wrap(const int nr, const int sum, const int wrap_limit) {
  */
 float wrap_float(const float nr, const float minimum, const float maximum) {
     const float diff = maximum - minimum;
-    return minimum + std::fmod(diff + std::fmod(nr - minimum, diff), diff);
+    return minimum + fmod(diff + fmod(nr - minimum, diff), diff);
 }
