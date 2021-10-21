7b7cedb114ddf8ec649f60f8b0ea7f2ddc25baf4
Espyo <andreluis.g.silva@gmail.com>
2021-10-21 19:29:17 +0100

    Manual page headers now include breadcrumbs and the engine's logo. Also, changed their JavaScript to automate more work.
    - The page's title, first heading, and breadcrumbs are now generated automatically via JavaScript, being fed only the title name and breadcrumb name + links.

--- Manual/data/script.js
+++ Manual/data/script.js
@@ -1,3 +1,6 @@
+/* Creates the table of contents based on the h2 and beyond tags.
+ * Then, it places it after the opening paragraphs.
+ */
 function make_toc () {
   var headings = [].slice.call(document.body.querySelectorAll('h2, h3, h4, h5, h6'));
   
@@ -47,4 +50,63 @@ function make_toc () {
     
     prev_level = level;
   }
+}
+
+
+/* Adjusts the header iframe to include breadcrumbs for the current page.
+ * title:
+ *   Current page title.
+ * bc_names:
+ *   List of names of the pages in the breadcrumbs. The home page must not be
+ *   included. Undefined for none.
+ * bc_links:
+ *   Same as bc_names, but for the links.
+ */
+function adjust_header(title, bc_names, bc_links) {
+  var iframe_doc = document.getElementById('header-frame').contentWindow.document;
+  var bc_span = iframe_doc.getElementById('breadcrumbs');
+  var result = '';
+  if(title != 'Home') {
+    result += '<a href="home.html" target="_parent">Home</a> &gt; ';
+  }
+  if(bc_names !== undefined || bc_links !== undefined) {
+    for(var b = 0; b < bc_names.length; b++) {
+      result += '<a href="' + bc_links[b] + '" target="_parent">' + bc_names[b] + '</a> &gt; ';
+    }
+  }
+  result += title;
+  bc_span.innerHTML = result;
+}
+
+
+/* Adjusts the page's title and first h1 header.
+ * title:
+ *   Current page title.
+ */
+function set_title(title) {
+  document.title = title + ' - Pikifen manual';
+  var title_h1 = document.createElement('h1');
+  title_h1.innerHTML = title;
+  var content_div = document.getElementById('content');
+  content_div.insertBefore(title_h1, content_div.firstChild);
+}
+
+
+/* Sets up the page.
+ * title:
+ *   Current page title.
+ * bc_names:
+ *   List of names of the pages in the breadcrumbs. The home page must not be
+ *   included. Undefined for none.
+ * bc_links:
+ *   Same as bc_names, but for the links.
+ * use_toc:
+ *   True to use table of contents.
+ */
+function setup(title, bc_names, bc_links, use_toc) {
+  if(use_toc === undefined) use_toc = true;
+
+  set_title(title);
+  adjust_header(title, bc_names, bc_links);
+  if(use_toc) make_toc();
 }
