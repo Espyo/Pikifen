Espyo/Pikifen

commit ec1458bd4c073b6c585d077bfff23299f8b78de8

Fixed some vertical movement+gravity logic, and fixed Winged Pikmin facing east to  move vertically.

Source/source/mobs/mob_physics.cpp
@@ -719,6 +719,10 @@ void mob::tick_rotation_physics(
 void mob::tick_vertical_movement_physics(
     const float delta_t, const float pre_move_ground_z
 ) {
+    bool apply_gravity = true;
+    float old_speed_z = speed_z;
+    speed_z = 0.0f;
+    
     if(!standing_on_mob) {
         //If the current ground is one step (or less) below
         //the previous ground, just instantly go down the step.
@@ -730,36 +734,32 @@ void mob::tick_vertical_movement_physics(
         }
     }
     
-    float move_speed_z = speed_z;
-    
     //Vertical chasing.
     if(
         chase_info.state == CHASE_STATE_CHASING &&
         can_move_in_midair &&
         (chase_info.flags & CHASE_FLAG_TELEPORT) == 0
     ) {
+        apply_gravity = false;
+        
         float target_z = chase_info.offset_z;
         if(chase_info.orig_z) target_z += *chase_info.orig_z;
         float diff_z = fabs(target_z - z);
-        float chase_speed_z =
+        
+        speed_z =
             std::min((float) (diff_z / delta_t), chase_info.cur_speed);
         if(target_z < z) {
-            chase_speed_z = -chase_speed_z;
+            speed_z = -speed_z;
         }
-        move_speed_z += chase_speed_z;
     }
     
     //Gravity.
-    if(
-        !can_move_in_midair &&
-        !holder.m
-    ) {
-        speed_z += delta_t* gravity_mult * GRAVITY_ADDER;
-        move_speed_z += speed_z;
+    if(apply_gravity && !can_move_in_midair && !holder.m) {
+        speed_z = old_speed_z + delta_t* gravity_mult * GRAVITY_ADDER;
     }
     
     //Apply the change in Z.
-    z += move_speed_z * delta_t;
+    z += speed_z * delta_t;
     
     //Landing.
     hazard* new_on_hazard = NULL;

Source/source/controls.cpp
@@ -647,7 +647,7 @@ void gameplay_state::handle_button(
         *   Move   --->/|  *
         *              V > *
         *******************/
-            
+        
         switch(button) {
         case BUTTON_RIGHT: {
             leader_movement.right = pos;
@@ -663,9 +663,9 @@ void gameplay_state::handle_button(
             break;
         }
         }
-
+        
         break;
-            
+        
     } case BUTTON_CURSOR_RIGHT:
     case BUTTON_CURSOR_UP:
     case BUTTON_CURSOR_LEFT:
@@ -675,7 +675,7 @@ void gameplay_state::handle_button(
         *   Cursor   ( = )> *
         *             '-'   *
         ********************/
-            
+        
         switch(button) {
         case BUTTON_CURSOR_RIGHT: {
             cursor_movement.right = pos;
@@ -691,9 +691,9 @@ void gameplay_state::handle_button(
             break;
         }
         }
-            
+        
         break;
-            
+        
     } case BUTTON_GROUP_RIGHT:
     case BUTTON_GROUP_UP:
     case BUTTON_GROUP_LEFT:
@@ -703,7 +703,7 @@ void gameplay_state::handle_button(
         *   Group   ****O *
         *            ***  *
         ******************/
-            
+        
         switch(button) {
         case BUTTON_GROUP_RIGHT: {
             swarm_movement.right = pos;
@@ -719,18 +719,18 @@ void gameplay_state::handle_button(
             break;
         }
         }
-            
+        
         break;
-            
+        
     } case BUTTON_GROUP_CURSOR: {
-    
-        swarm_cursor = is_down;
-            
-        break;
-            
-    } 
-    }
 
+        swarm_cursor = is_down;
+        
+        break;
+        
+    }
+    }
+    
 }
 
 

Source/source/mobs/mob.cpp
@@ -2487,7 +2487,7 @@ void mob::tick_brain(const float delta_t) {
             //time to make it think about how to get there.
             
             //Let the mob think about facing the actual target.
-            if(!type->can_free_move) {
+            if(!type->can_free_move && horiz_dist > 0.0f) {
                 face(get_angle(pos, final_target_pos), NULL);
             }
             

commit f7d8d54480cc801af837f4d03e638b86ab631b62

Fixed maximum throw heights not being respected.
- This got broken with ec1458bd.
- Turns out that before that commit, all vertical movement calculations were twice as strong as they should've been, but it was hard to notice with throws since there's a Z cap. Now that the physics are more correct, the throws fail because of floating point inaccuracies compounding over time.
- To help with this, gravity is now calculated using Velocity Verlet instead of Euler's Method, and all thrown objects receive a small bump in vertical throw speed.

Source/source/mobs/leader.cpp
@@ -942,6 +942,10 @@ void leader::update_throw_variables() {
     }
     }
     
+    //Due to floating point inaccuracies, it's hard for mobs to actually
+    //reach the intended value. Let's bump it up just a smidge.
+    max_height += 0.5f;
+    
     if(max_height >= (target_z - z)) {
         //Can reach.
         throwee_can_reach = true;

Source/source/mobs/mob_physics.cpp
@@ -737,8 +737,6 @@ void mob::tick_vertical_movement_physics(
     const bool was_teleport
 ) {
     bool apply_gravity = true;
-    float old_speed_z = speed_z;
-    speed_z = 0.0f;
     
     if(!standing_on_mob) {
         //If the current ground is one step (or less) below
@@ -768,6 +766,8 @@ void mob::tick_vertical_movement_physics(
         if(target_z < z) {
             speed_z = -speed_z;
         }
+        
+        z += speed_z * delta_t;
     }
     
     //Gravity.
@@ -775,11 +775,13 @@ void mob::tick_vertical_movement_physics(
         apply_gravity && !has_flag(flags, MOB_FLAG_CAN_MOVE_MIDAIR) &&
         !holder.m && !was_teleport
     ) {
-        speed_z = old_speed_z + delta_t* gravity_mult * MOB::GRAVITY_ADDER;
+        //Use Velocity Verlet for better results.
+        //https://youtu.be/hG9SzQxaCm8
+        z +=
+            (speed_z * delta_t) +
+            ((MOB::GRAVITY_ADDER / 2.0f) * delta_t* delta_t);
+        speed_z += MOB::GRAVITY_ADDER * delta_t;
     }
-    
-    //Apply the change in Z.
-    z += speed_z * delta_t;
     
     //Landing.
     hazard* new_on_hazard = NULL;