Espyo/Pikifen

commit 366acca6428e23f5d2fc87c39d2971ac23328451

mobs that extend past their radius no longer stop rendering when partially offscreen (#41)
Co-authored-by: Kaelan B <43509068+Helodity@users.noreply.github.com>

Source/source/mobs/mob.cpp
@@ -2529,17 +2529,16 @@ void mob::hold(
  */
 bool mob::is_off_camera() const {
     if(parent) return false;
-    
-    float radius_to_use;
-    if(rectangular_dim.x == 0) {
-        radius_to_use = radius;
-    } else {
-        radius_to_use =
-            std::max(
-                rectangular_dim.x / 2.0,
-                rectangular_dim.y / 2.0
-            );
-    }
+
+    sprite* sprite = anim.get_cur_sprite();
+    if(!sprite) return true;
+
+    point sprite_size = sprite->file_size;
+    float radius_to_use =
+        std::max(
+            sprite_size.x / 2.0,
+            sprite_size.y / 2.0
+        );
     
     return !bbox_check(game.cam.box[0], game.cam.box[1], pos, radius_to_use);
 }

commit 6700e708d75e6150d5765e5c2e25486d61f17a8d

Fixed bug where bridges do not render
Introduced from 366acca6428e23f5d2fc87c39d2971ac23328451

Source/source/mobs/mob.cpp
@@ -2530,16 +2530,29 @@ void mob::hold(
 bool mob::is_off_camera() const {
     if(parent) return false;
 
+    float sprite_bound = 0;
     sprite* sprite = anim.get_cur_sprite();
-    if(!sprite) return true;
+    if(sprite) {
+        point sprite_size = sprite->file_size;
+        sprite_bound =
+            std::max(
+                sprite_size.x / 2.0,
+                sprite_size.y / 2.0
+            );
+    }
 
-    point sprite_size = sprite->file_size;
-    float radius_to_use =
-        std::max(
-            sprite_size.x / 2.0,
-            sprite_size.y / 2.0
-        );
-    
+    float collision_bound = 0;
+    if(rectangular_dim.x == 0) {
+        collision_bound = radius;
+    } else {
+        collision_bound =
+            std::max(
+                rectangular_dim.x / 2.0,
+                rectangular_dim.y / 2.0
+            );
+    }
+
+    float radius_to_use = std::max(sprite_bound, collision_bound);
     return !bbox_check(game.cam.box[0], game.cam.box[1], pos, radius_to_use);
 }
 

Source/visual_studio_2019/Pikifen.vcxproj
@@ -28,26 +28,26 @@
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|Win32'" Label="Configuration">
     <ConfigurationType>Application</ConfigurationType>
     <UseDebugLibraries>true</UseDebugLibraries>
-    <PlatformToolset>v142</PlatformToolset>
+    <PlatformToolset>v143</PlatformToolset>
     <CharacterSet>MultiByte</CharacterSet>
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|Win32'" Label="Configuration">
     <ConfigurationType>Application</ConfigurationType>
     <UseDebugLibraries>false</UseDebugLibraries>
-    <PlatformToolset>v142</PlatformToolset>
+    <PlatformToolset>v143</PlatformToolset>
     <WholeProgramOptimization>true</WholeProgramOptimization>
     <CharacterSet>MultiByte</CharacterSet>
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Debug|x64'" Label="Configuration">
     <ConfigurationType>Application</ConfigurationType>
     <UseDebugLibraries>true</UseDebugLibraries>
-    <PlatformToolset>v142</PlatformToolset>
+    <PlatformToolset>v143</PlatformToolset>
     <CharacterSet>MultiByte</CharacterSet>
   </PropertyGroup>
   <PropertyGroup Condition="'$(Configuration)|$(Platform)'=='Release|x64'" Label="Configuration">
     <ConfigurationType>Application</ConfigurationType>
     <UseDebugLibraries>false</UseDebugLibraries>
-    <PlatformToolset>v142</PlatformToolset>
+    <PlatformToolset>v143</PlatformToolset>
     <WholeProgramOptimization>true</WholeProgramOptimization>
     <CharacterSet>MultiByte</CharacterSet>
   </PropertyGroup>