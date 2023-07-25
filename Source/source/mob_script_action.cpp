5bbcaa95fb1309005f128fffb344c95166379f5e
Kaelan B <43509068+Helodity@users.noreply.github.com>
2023-07-25 01:28:40 -0400

    `get_info mob_type` and `get_info mob_category` now work with `on_finish_receiving_delivery ` and `on_start_receiving_delivery

--- Source/source/mob_script_action.cpp
+++ Source/source/mob_script_action.cpp
@@ -2274,7 +2274,9 @@ void get_info_runner(mob_action_run_data &data, mob* target_mob) {
             data.call->parent_event == MOB_EV_TOUCHED_OPPONENT ||
             data.call->parent_event == MOB_EV_OBJECT_IN_REACH ||
             data.call->parent_event == MOB_EV_OPPONENT_IN_REACH ||
-            data.call->parent_event == MOB_EV_HELD
+            data.call->parent_event == MOB_EV_HELD ||
+            data.call->parent_event == MOB_EV_STARTED_RECEIVING_DELIVERY ||
+            data.call->parent_event == MOB_EV_FINISHED_RECEIVING_DELIVERY
         ) {
             *var = ((mob*)(data.custom_data_1))->type->category->name;
         }
@@ -2286,7 +2288,9 @@ void get_info_runner(mob_action_run_data &data, mob* target_mob) {
             data.call->parent_event == MOB_EV_TOUCHED_OPPONENT ||
             data.call->parent_event == MOB_EV_OBJECT_IN_REACH ||
             data.call->parent_event == MOB_EV_OPPONENT_IN_REACH ||
-            data.call->parent_event == MOB_EV_THROWN_PIKMIN_LANDED
+            data.call->parent_event == MOB_EV_THROWN_PIKMIN_LANDED ||
+            data.call->parent_event == MOB_EV_STARTED_RECEIVING_DELIVERY ||
+            data.call->parent_event == MOB_EV_FINISHED_RECEIVING_DELIVERY
         ) {
             *var = ((mob*)(data.custom_data_1))->type->name;
         }
