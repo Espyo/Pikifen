Espyo/Pikifen

commit 8c229929b82697de5ecf47c8cbf8e9c2d3cd5ecc

Carrying related fixes
- Fixed ship's control point and receptacle not moving with the ship
- Fixed carriable mobs going to a mob's old location

Source/source/mobs/mob.cpp
@@ -3458,65 +3458,75 @@ void mob::tick_brain(const float delta_t) {
             
         } else {
             //Reached the chase location.
-            bool direct =
-                path_info &&
-                (
-                    path_info->result == PATH_RESULT_DIRECT ||
-                    path_info->result == PATH_RESULT_DIRECT_NO_STOPS
-                );
-            if(
-                path_info && !direct &&
-                path_info->block_reason == PATH_BLOCK_REASON_NONE
-            ) {
-            
-                path_info->cur_path_stop_nr++;
-                
-                if(path_info->cur_path_stop_nr < path_info->path.size()) {
-                    //Reached a regular stop while traversing the path.
-                    //Think about going to the next, if possible.
-                    if(path_info->check_blockage(&path_info->block_reason)) {
-                        //Oop, there's an obstacle! Or some other blockage.
-                        fsm.run_event(MOB_EV_PATH_BLOCKED);
-                    } else {
-                        //All good. Head to the next stop.
-                        path_stop* next_stop =
-                            path_info->path[path_info->cur_path_stop_nr];
-                        float next_stop_z = z;
-                        if(
-                            (
-                                path_info->settings.flags &
-                                PATH_FOLLOW_FLAG_AIRBORNE
-                            ) &&
-                            next_stop->sector_ptr
+
+            if(path_info) {
+                //Our mob moved! Recalculate!
+                if (
+                    carry_info && carry_info->intended_mob && 
+                    carry_info->intended_mob->pos != carry_info->intended_point
+                ) {
+                    carry_info->intended_point = carry_info->intended_mob->pos;
+                    fsm.run_event(MOB_EV_PATHS_CHANGED);
+                } else {
+                    bool direct =
+                        path_info->result == PATH_RESULT_DIRECT ||
+                        path_info->result == PATH_RESULT_DIRECT_NO_STOPS;
+
+                    if(
+                        !direct && 
+                        path_info->block_reason == PATH_BLOCK_REASON_NONE
+                    ) {
+                        path_info->cur_path_stop_nr++;
+
+                        if(path_info->cur_path_stop_nr < path_info->path.size()) {
+                            //Reached a regular stop while traversing the path.
+                            //Think about going to the next, if possible.
+                            if(path_info->check_blockage(&path_info->block_reason)) {
+                                //Oop, there's an obstacle! Or some other blockage.
+                                fsm.run_event(MOB_EV_PATH_BLOCKED);
+                            } else {
+                                //All good. Head to the next stop.
+                                path_stop* next_stop =
+                                    path_info->path[path_info->cur_path_stop_nr];
+                                float next_stop_z = z;
+                                if(
+                                    has_flag(
+                                        path_info->settings.flags, 
+                                        PATH_FOLLOW_FLAG_AIRBORNE
+                                    ) &&
+                                    next_stop->sector_ptr
+                                ) {
+                                    next_stop_z =
+                                        next_stop->sector_ptr->z +
+                                        pikmin::FLIER_ABOVE_FLOOR_HEIGHT;
+                                }
+
+                                chase(
+                                    next_stop->pos, next_stop_z,
+                                    CHASE_FLAG_ANY_ANGLE,
+                                    MOB::DEF_CHASE_TARGET_DISTANCE,
+                                    chase_info.max_speed
+                                );
+                            }
+
+                        } else if(
+                            path_info->cur_path_stop_nr == path_info->path.size()
                         ) {
-                            next_stop_z =
-                                next_stop->sector_ptr->z +
-                                pikmin::FLIER_ABOVE_FLOOR_HEIGHT;
+                            //Reached the final stop of the path, but not the goal.
+                            //Let's head there.
+                            move_to_path_end(
+                                chase_info.max_speed, chase_info.acceleration
+                            );
+
+                        } else if(
+                            path_info->cur_path_stop_nr == path_info->path.size() + 1
+                        ) {
+                            //Reached the path's goal.
+                            chase_info.state = CHASE_STATE_FINISHED;
                         }
-                        
-                        chase(
-                            next_stop->pos, next_stop_z,
-                            CHASE_FLAG_ANY_ANGLE,
-                            MOB::DEF_CHASE_TARGET_DISTANCE,
-                            chase_info.max_speed
-                        );
+                    } else {
+                        chase_info.state = CHASE_STATE_FINISHED;
                     }
-                    
-                } else if(
-                    path_info->cur_path_stop_nr == path_info->path.size()
-                ) {
-                    //Reached the final stop of the path, but not the goal.
-                    //Let's head there.
-                    move_to_path_end(
-                        chase_info.max_speed, chase_info.acceleration
-                    );
-                    
-                } else if(
-                    path_info->cur_path_stop_nr == path_info->path.size() + 1
-                ) {
-                    //Reached the path's goal.
-                    chase_info.state = CHASE_STATE_FINISHED;
-                    
                 }
                 
             } else {

Source/source/mobs/ship.cpp
@@ -279,5 +279,6 @@ void ship::tick_class_specifics(const float delta_t) {
             r++;
         }
     }
-    
+    control_point_final_pos = rotate_point(shi_type->control_point_offset, angle) + pos;
+    receptacle_final_pos = rotate_point(shi_type->receptacle_offset, angle) + pos;
 }

commit 591c0b53931e070949092f6076e8be6f614e7e48

More carrying fixes
Fixed bug introduced in 8c229929, where pikmin did not respect ship's control point
Choosing a target now respects obstacles and path distance

Source/source/mob_fsms/gen_mob_fsm.cpp
@@ -428,6 +428,29 @@ void gen_mob_fsm::handle_delivery(mob* m, void* info1, void* info2) {
 
 
 /* ----------------------------------------------------------------------------
+ * Generic handler for when a mob was delivered to an Onion/ship.
+ * m:
+ *   The mob.
+ * info1:
+ *   Unused.
+ * info2:
+ *   Unused.
+ */
+void gen_mob_fsm::handle_path_changed(mob* m, void* info1, void* info2) {
+    mob* prev_mob = m->carry_info->intended_mob;
+    pikmin_type* prev_pik_type = m->carry_info->intended_pik_type;
+    m->calculate_carrying_destination(
+        NULL, NULL,
+        &m->carry_info->intended_pik_type,
+        &m->carry_info->intended_mob, &m->carry_info->intended_point
+    );
+
+    //Send a move begin event to follow the new path.
+    m->fsm.run_event(MOB_EV_CARRY_BEGIN_MOVE);
+}
+
+
+/* ----------------------------------------------------------------------------
  * When a mob has to lose its momentum.
  * m:
  *   The mob.

Source/source/mob_fsms/gen_mob_fsm.h
@@ -30,6 +30,7 @@ void fall_down_pit(mob* m, void* info1, void* info2);
 void handle_carrier_added(mob* m, void* info1, void* info2);
 void handle_carrier_removed(mob* m, void* info1, void* info2);
 void handle_delivery(mob* m, void* info1, void* info2);
+void handle_path_changed(mob* m, void* info1, void* info2);
 void lose_momentum(mob* m, void* info1, void* info2);
 void start_being_delivered(mob* m, void* info1, void* info2);
 void touch_hazard(mob* m, void* info1, void* info2);

Source/source/mob_fsms/leader_fsm.cpp
@@ -735,8 +735,7 @@ void leader_fsm::create_fsm(mob_type* typ) {
             efc.change_state("sleeping_stuck");
         }
         efc.new_event(MOB_EV_PATHS_CHANGED); {
-            efc.run(gen_mob_fsm::carry_get_path);
-            efc.run(gen_mob_fsm::carry_begin_move);
+            efc.run(gen_mob_fsm::handle_path_changed);
         }
         efc.new_event(MOB_EV_REACHED_DESTINATION); {
             efc.run(gen_mob_fsm::carry_reach_destination);
@@ -795,9 +794,7 @@ void leader_fsm::create_fsm(mob_type* typ) {
             efc.change_state("sleeping_waiting");
         }
         efc.new_event(MOB_EV_PATHS_CHANGED); {
-            efc.run(gen_mob_fsm::carry_stop_being_stuck);
-            efc.run(gen_mob_fsm::carry_get_path);
-            efc.change_state("sleeping_moving");
+            efc.run(gen_mob_fsm::handle_path_changed);
         }
         efc.new_event(LEADER_EV_CANCEL); {
             efc.run(gen_mob_fsm::carry_stop_being_stuck);
@@ -899,8 +896,7 @@ void leader_fsm::create_fsm(mob_type* typ) {
             efc.change_state("inactive_sleeping_stuck");
         }
         efc.new_event(MOB_EV_PATHS_CHANGED); {
-            efc.run(gen_mob_fsm::carry_get_path);
-            efc.run(gen_mob_fsm::carry_begin_move);
+            efc.run(gen_mob_fsm::handle_path_changed);
         }
         efc.new_event(MOB_EV_REACHED_DESTINATION); {
             efc.run(gen_mob_fsm::carry_reach_destination);
@@ -963,9 +959,7 @@ void leader_fsm::create_fsm(mob_type* typ) {
             efc.change_state("inactive_sleeping_waiting");
         }
         efc.new_event(MOB_EV_PATHS_CHANGED); {
-            efc.run(gen_mob_fsm::carry_stop_being_stuck);
-            efc.run(gen_mob_fsm::carry_get_path);
-            efc.change_state("inactive_sleeping_moving");
+            efc.run(gen_mob_fsm::handle_path_changed);
         }
         efc.new_event(LEADER_EV_CANCEL); {
             efc.run(gen_mob_fsm::carry_stop_being_stuck);

Source/source/mob_fsms/pellet_fsm.cpp
@@ -68,8 +68,7 @@ void pellet_fsm::create_fsm(mob_type* typ) {
             efc.change_state("idle_stuck");
         }
         efc.new_event(MOB_EV_PATHS_CHANGED); {
-            efc.run(gen_mob_fsm::carry_get_path);
-            efc.run(gen_mob_fsm::carry_begin_move);
+            efc.run(gen_mob_fsm::handle_path_changed);
         }
         efc.new_event(MOB_EV_CARRY_DELIVERED); {
             efc.change_state("being_delivered");
@@ -99,9 +98,7 @@ void pellet_fsm::create_fsm(mob_type* typ) {
             efc.change_state("idle_waiting");
         }
         efc.new_event(MOB_EV_PATHS_CHANGED); {
-            efc.run(gen_mob_fsm::carry_stop_being_stuck);
-            efc.run(gen_mob_fsm::carry_get_path);
-            efc.change_state("idle_moving");
+            efc.run(gen_mob_fsm::handle_path_changed);
         }
     }
     

Source/source/mob_fsms/resource_fsm.cpp
@@ -74,8 +74,7 @@ void resource_fsm::create_fsm(mob_type* typ) {
             efc.change_state("idle_stuck");
         }
         efc.new_event(MOB_EV_PATHS_CHANGED); {
-            efc.run(gen_mob_fsm::carry_get_path);
-            efc.run(gen_mob_fsm::carry_begin_move);
+            efc.run(gen_mob_fsm::handle_path_changed);
         }
         efc.new_event(MOB_EV_CARRY_DELIVERED); {
             efc.change_state("being_delivered");
@@ -106,9 +105,7 @@ void resource_fsm::create_fsm(mob_type* typ) {
             efc.change_state("idle_waiting");
         }
         efc.new_event(MOB_EV_PATHS_CHANGED); {
-            efc.run(gen_mob_fsm::carry_stop_being_stuck);
-            efc.run(gen_mob_fsm::carry_get_path);
-            efc.change_state("idle_moving");
+            efc.run(gen_mob_fsm::handle_path_changed);
         }
     }
     

Source/source/mob_fsms/treasure_fsm.cpp
@@ -71,8 +71,7 @@ void treasure_fsm::create_fsm(mob_type* typ) {
             efc.change_state("idle_stuck");
         }
         efc.new_event(MOB_EV_PATHS_CHANGED); {
-            efc.run(gen_mob_fsm::carry_get_path);
-            efc.run(gen_mob_fsm::carry_begin_move);
+            efc.run(gen_mob_fsm::handle_path_changed);
         }
         efc.new_event(MOB_EV_BOTTOMLESS_PIT); {
             efc.run(treasure_fsm::respawn);
@@ -102,9 +101,7 @@ void treasure_fsm::create_fsm(mob_type* typ) {
             efc.change_state("idle_waiting");
         }
         efc.new_event(MOB_EV_PATHS_CHANGED); {
-            efc.run(gen_mob_fsm::carry_stop_being_stuck);
-            efc.run(gen_mob_fsm::carry_get_path);
-            efc.change_state("idle_moving");
+            efc.run(gen_mob_fsm::handle_path_changed);
         }
         efc.new_event(MOB_EV_BOTTOMLESS_PIT); {
             efc.run(treasure_fsm::respawn);

Source/source/mob_types/mob_type.cpp
@@ -151,8 +151,7 @@ void mob_type::add_carrying_states() {
             efc.change_state("carriable_stuck");
         }
         efc.new_event(MOB_EV_PATHS_CHANGED); {
-            efc.run(gen_mob_fsm::carry_get_path);
-            efc.run(gen_mob_fsm::carry_begin_move);
+            efc.run(gen_mob_fsm::handle_path_changed);
         }
         efc.new_event(MOB_EV_CARRY_DELIVERED); {
             efc.change_state("being_delivered");
@@ -182,9 +181,7 @@ void mob_type::add_carrying_states() {
             efc.change_state("carriable_waiting");
         }
         efc.new_event(MOB_EV_PATHS_CHANGED); {
-            efc.run(gen_mob_fsm::carry_stop_being_stuck);
-            efc.run(gen_mob_fsm::carry_get_path);
-            efc.change_state("carriable_moving");
+            efc.run(gen_mob_fsm::handle_path_changed);
         }
     }
     

Source/source/mobs/mob.cpp
@@ -623,32 +623,16 @@ bool mob::calculate_carrying_destination(
     *target_point = pos;
     if(!carry_info) return false;
     
+    vector<mob*> potential_mobs;
+
     switch(carry_info->destination) {
     case CARRY_DESTINATION_SHIP: {
 
-        //Go to the nearest ship.
-        ship* closest_ship = NULL;
-        dist closest_ship_dist;
-        
+        //All ships are potential targets
         for(size_t s = 0; s < game.states.gameplay->mobs.ships.size(); ++s) {
             ship* s_ptr = game.states.gameplay->mobs.ships[s];
-            dist d(pos, s_ptr->control_point_final_pos);
-            
-            if(!closest_ship || d < closest_ship_dist) {
-                closest_ship = s_ptr;
-                closest_ship_dist = d;
-            }
+            potential_mobs.push_back(s_ptr);
         }
-        
-        if(closest_ship) {
-            *target_mob = closest_ship;
-            *target_point = closest_ship->control_point_final_pos;
-            return true;
-            
-        } else {
-            return false;
-        }
-        
         break;
         
     } case CARRY_DESTINATION_ONION: {
@@ -658,16 +642,15 @@ bool mob::calculate_carrying_destination(
         unordered_set<pikmin_type*> available_types;
         for(size_t o = 0; o < game.states.gameplay->mobs.onions.size(); o++) {
             onion* o_ptr = game.states.gameplay->mobs.onions[o];
-            if(o_ptr->activated) {
-                for(
-                    size_t t = 0;
-                    t < o_ptr->oni_type->nest->pik_types.size();
-                    ++t
-                ) {
-                    available_types.insert(
-                        o_ptr->oni_type->nest->pik_types[t]
-                    );
-                }
+            if(!o_ptr->activated) continue;
+            for(
+                size_t t = 0;
+                t < o_ptr->oni_type->nest->pik_types.size();
+                ++t
+            ) {
+                available_types.insert(
+                    o_ptr->oni_type->nest->pik_types[t]
+                );
             }
         }
         
@@ -678,75 +661,35 @@ bool mob::calculate_carrying_destination(
         
         pikmin_type* decided_type =
             decide_carry_pikmin_type(available_types, added, removed);
-            
-        //Figure out where that type's Onion is.
-        size_t closest_onion_nr = INVALID;
-        dist closest_onion_dist;
+        *target_type = decided_type;
+
         for(size_t o = 0; o < game.states.gameplay->mobs.onions.size(); ++o) {
             onion* o_ptr = game.states.gameplay->mobs.onions[o];
             if(!o_ptr->activated) continue;
-            bool has_type = false;
-            for(
-                size_t t = 0;
-                t < o_ptr->oni_type->nest->pik_types.size();
-                ++t
-            ) {
-                if(o_ptr->oni_type->nest->pik_types[t] == decided_type) {
-                    has_type = true;
-                    break;
-                }
-            }
-            if(!has_type) continue;
-            
-            dist d(pos, o_ptr->pos);
-            if(closest_onion_nr == INVALID || d < closest_onion_dist) {
-                closest_onion_dist = d;
-                closest_onion_nr = o;
+            bool has_type = 
+                std::find(
+                    o_ptr->oni_type->nest->pik_types.begin(),
+                    o_ptr->oni_type->nest->pik_types.end(),
+                    decided_type
+                ) != o_ptr->oni_type->nest->pik_types.end();
+
+            if(has_type) {
+                potential_mobs.push_back(o_ptr);
             }
         }
-        
-        //Finally, set the destination data.
-        *target_type = decided_type;
-        *target_mob = game.states.gameplay->mobs.onions[closest_onion_nr];
-        *target_point = (*target_mob)->pos;
-        
-        return true;
-        
         break;
         
     } case CARRY_DESTINATION_LINKED_MOB: {
-
-        //If it's towards a linked mob, just go to the closest one.
-        mob* closest_link = NULL;
-        dist closest_link_dist;
-
         for(size_t s = 0; s < links.size(); ++s) {
-            dist d(pos, links[s]->pos);
-
-            if(!closest_link || d < closest_link_dist) {
-                closest_link = links[s];
-                closest_link_dist = d;
+            if(links[s]) {
+                potential_mobs.push_back(links[s]);
             }
         }
-
-        if(closest_link) {
-            *target_mob = closest_link;
-            *target_point = closest_link->pos;
-            return true;
-
-        } else {
-            return false;
-        }
-
         break;
         
     } case CARRY_DESTINATION_LINKED_MOB_MATCHING_TYPE: {
 
         //Towards one of the linked mobs that matches the decided Pikmin type.
-        if(links.empty()) {
-            return false;
-        }
-        
         unordered_set<pikmin_type*> available_types;
         vector<std::pair<mob*, pikmin_type*> > mobs_per_type;
         
@@ -774,30 +717,67 @@ bool mob::calculate_carrying_destination(
         
         pikmin_type* decided_type =
             decide_carry_pikmin_type(available_types, added, removed);
-            
+        *target_type = decided_type;
+
         //Figure out which linked mob matches the decided type.
-        size_t closest_target_idx = INVALID;
-        dist closest_target_dist;
         for(size_t m = 0; m < mobs_per_type.size(); ++m) {
             if(mobs_per_type[m].second != decided_type) continue;
-            
-            dist d(pos, mobs_per_type[m].first->pos);
-            if(closest_target_idx == INVALID || d < closest_target_dist) {
-                closest_target_dist = d;
-                closest_target_idx = m;
-            }
+
+            potential_mobs.push_back(mobs_per_type[m].first);
         }
-        
-        //Finally, set the destination data.
-        *target_type = decided_type;
-        *target_mob = links[closest_target_idx];
-        *target_point = (*target_mob)->pos;
-        
-        return true;
-        
         break;
         
     }
+    }
+
+    //Use some temporary settings so we can determine whether paths are blocked
+    path_follow_settings temp_settings;
+
+    //Check if this carriable is considered light load.
+    if(type->weight == 1) {
+        enable_flag(temp_settings.flags, PATH_FOLLOW_FLAG_LIGHT_LOAD);
+    }
+    //The object will only be airborne if all its carriers can fly.
+    if(carry_info->can_fly()) {
+        enable_flag(temp_settings.flags, PATH_FOLLOW_FLAG_AIRBORNE);
+    }
+
+    //The object is only as invulnerable as the Pikmin carrying it.
+    temp_settings.invulnerabilities =
+        carry_info->get_carrier_invulnerabilities();
+
+    mob* closest_mob = NULL;
+    float closest_dist;
+    bool closest_blocked = false;
+
+    for(size_t m = 0; m < potential_mobs.size(); ++m) {
+        temp_settings.target_mob = potential_mobs[m];
+        temp_settings.target_point = potential_mobs[m]->get_delivery_point();
+
+        vector<path_stop*> path;
+        float dist;
+        PATH_RESULTS results = get_path(
+            pos, temp_settings.target_point, temp_settings,
+            path, &dist, NULL, NULL
+        );
+        bool is_blocked = 
+            results < 0 || 
+            results == PATH_RESULT_PATH_WITH_OBSTACLES;
+        //If we have an unblocked destination, ignore this one.
+        if(is_blocked && !closest_blocked) continue;
+
+        //If we don't have an unblocked destination and we find one, make that our target.
+        if(!closest_mob || dist < closest_dist || !is_blocked && closest_blocked) {
+            closest_dist = dist;
+            closest_mob = potential_mobs[m];
+            closest_blocked = is_blocked;
+        }
+    }
+
+    if(closest_mob) {
+        *target_mob = closest_mob;
+        *target_point = closest_mob->get_delivery_point();
+        return true;
     }
     
     return false;
@@ -1197,6 +1177,20 @@ pikmin_type* mob::decide_carry_pikmin_type(
     const unordered_set<pikmin_type*> &available_types,
     mob* added, mob* removed
 ) const {
+    //We didn't change the pikmin type we have
+    //so don't change the target
+    if(!added && !removed) {
+        if(
+            std::find(
+                available_types.begin(),
+                available_types.end(),
+                carry_info->intended_pik_type
+            ) == available_types.end()
+        ) {
+            return carry_info->intended_pik_type;
+        }
+    }
+
     //How many of each Pikmin type are carrying.
     map<pikmin_type*, unsigned> type_quantity;
     //The Pikmin type with the most carriers.
@@ -1799,6 +1793,15 @@ hitbox* mob::get_closest_hitbox(
     if(d) *d = closest_hitbox_dist;
     
     return closest_hitbox;
+}
+
+
+/* ----------------------------------------------------------------------------
+ * Returns where deliveries to this mob should go.
+ * Meant for children to override this.
+ */
+point mob::get_delivery_point() {
+    return pos;
 }
 
 
@@ -3488,9 +3491,8 @@ void mob::tick_brain(const float delta_t) {
                 //Our mob moved! Recalculate!
                 if (
                     carry_info && carry_info->intended_mob && 
-                    carry_info->intended_mob->pos != carry_info->intended_point
+                    carry_info->intended_mob->get_delivery_point() != carry_info->intended_point
                 ) {
-                    carry_info->intended_point = carry_info->intended_mob->pos;
                     fsm.run_event(MOB_EV_PATHS_CHANGED);
                 } else {
                     bool direct =

Source/source/mobs/mob.h
@@ -313,6 +313,7 @@ public:
     ) const;
     void cause_spike_damage(mob* victim, const bool is_ingestion);
     void chomp(mob* m, hitbox* hitbox_info);
+    virtual point get_delivery_point();
     sprite* get_cur_sprite() const;
     void get_hitbox_hold_point(
         mob* mob_to_hold, hitbox* h_ptr,

Source/source/mobs/ship.cpp
@@ -204,6 +204,11 @@ void ship::draw_mob() {
 }
 
 
+point ship::get_delivery_point() {
+    return control_point_final_pos;
+}
+
+
 /* ----------------------------------------------------------------------------
 * Heals a leader, causes particle effects, etc.
 * l:

Source/source/mobs/ship.h
@@ -57,6 +57,9 @@ public:
     //Checks if a leader is on the control point.
     bool is_leader_on_cp(leader* l) const;
     
+    //Get treasure delivery location.
+    point get_delivery_point() override;
+
     //Constructor.
     ship(const point &pos, ship_type* type, float angle);
     //Destructor.