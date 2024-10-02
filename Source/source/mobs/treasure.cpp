2dafbfca050cfea88c81f59040c04a0283325e38
Espyo <andreluis.g.silva@gmail.com>
2024-10-02 18:59:54 +0100

    Treasures now emit sparkle particles.

--- Source/source/mobs/treasure.cpp
+++ Source/source/mobs/treasure.cpp
@@ -34,4 +34,21 @@ treasure::treasure(const point &pos, treasure_type* type, const float angle) :
         MOB_TYPE::ANIM_IDLING, START_ANIM_OPTION_RANDOM_TIME_ON_SPAWN, true
     );
     
+    particle pa(
+        PARTICLE_TYPE_BITMAP, pos, z + height + 1.0f,
+        28.0f, 1.0f, PARTICLE_PRIORITY_LOW
+    );
+    pa.bitmap = game.sys_assets.bmp_sparkle;
+    pa.speed.y = -30.0f;
+    particle_generator pg(0.4f, pa);
+    pg.id = MOB_PARTICLE_GENERATOR_ID_SCRIPT;
+    pg.follow_mob = this;
+    pg.follow_angle = &this->angle;
+    pg.follow_z_offset = z + height + 1.0f;
+    pg.duration_deviation = 0.1f;
+    pg.interval_deviation = 0.05f;
+    pg.pos_deviation = point(radius * 0.75f, radius * 0.75f);
+    pg.size_deviation = 0.5f;
+    particle_generators.push_back(pg);
+    
 }
