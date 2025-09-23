1439f4c479b710d53b43b3ade61ad50bde82661d
Kaelan Bartlett <kbartlettofficial@gmail.com>
2025-09-23 13:45:12 -0400

    Update onion menus to use a swirl shader (#85)
    * Added an onion swirl shader. Used by onion menus

--- source/source/content/mob_type/onion_type.cpp
+++ source/source/content/mob_type/onion_type.cpp
@@ -73,6 +73,7 @@ AnimConversionVector OnionType::getAnimConversions() const {
  */
 void OnionType::loadCatProperties(DataNode* file) {
     nest->loadProperties(file, this);
+    nest->createColormap();
     
     for(size_t s = 0; s < sounds.size(); s++) {
         if(sounds[s].name == "pop") {
--- source/source/game_state/gameplay/drawing.cpp
+++ source/source/game_state/gameplay/drawing.cpp
@@ -1425,13 +1425,30 @@ void GameplayState::drawLightingFilter(const Viewport& view) {
  * @brief Draws the current Onion menu.
  */
 void GameplayState::drawOnionMenu() {
-    al_draw_filled_rectangle(
-        0, 0, game.winW, game.winH,
-        al_map_rgba(24, 64, 60, 220 * onionMenu->bgAlphaMult)
-    );
-    
+    ALLEGRO_SHADER* menuShader = game.shaders.getShader(SHADER_TYPE_ONION);
+
+    if (menuShader) {
+        //Using a shader requires drawing a bitmap
+        ALLEGRO_BITMAP* bmp = al_create_bitmap(game.winW, game.winH);
+        al_use_shader(menuShader);
+        al_set_shader_sampler("colormap", onionMenu->nestPtr->nestType->menuColormap, 1);
+        al_set_shader_float("area_time", game.timePassed);
+        al_set_shader_float("brightness", 0.4f);
+        al_set_shader_float("opacity", 0.8f * onionMenu->bgAlphaMult);
+
+        al_draw_bitmap(bmp, 0, 0, 0);
+        al_use_shader(NULL);
+        al_destroy_bitmap(bmp);
+    }
+    else {
+        al_draw_filled_rectangle(
+            0, 0, game.winW, game.winH,
+            al_map_rgba(24, 64, 60, 220 * onionMenu->bgAlphaMult)
+        );
+    }
+
     onionMenu->gui.draw();
-    
+
     drawMouseCursor(GAME::CURSOR_STANDARD_COLOR);
 }
 
